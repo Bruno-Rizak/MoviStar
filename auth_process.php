2a36270b376ab9c9a1852fe281d0728360e1ce80
Bruno-Rizak <brunorizak@gmail.com>
2024-02-15 22:46:06 -0300

    Ajustes na autenticação de login

--- auth_process.php
+++ auth_process.php
@@ -1,4 +1,5 @@
 <?php
+
   require_once("globals.php");
   require_once("db.php");
   require_once("models/User.php");
@@ -8,30 +9,28 @@
   $message = new Message($BASE_URL);
 
   $userDao = new UserDAO($conn, $BASE_URL);
-  
 
-  // Resgate o tipo do formulario
+  // Resgata o tipo do formulário
   $type = filter_input(INPUT_POST, "type");
 
-  // Verificar o tipo do formulario
+  // Verificação do tipo de formulário
+  if($type === "register") {
 
-  if($type === "register"){
     $name = filter_input(INPUT_POST, "name");
     $lastname = filter_input(INPUT_POST, "lastname");
     $email = filter_input(INPUT_POST, "email");
     $password = filter_input(INPUT_POST, "password");
     $confirmpassword = filter_input(INPUT_POST, "confirmpassword");
 
-    // Verificação de dados minimos
-    if($name && $lastname && $email && $password){
+    // Verificação de dados mínimos 
+    if($name && $lastname && $email && $password) {
 
       // Verificar se as senhas batem
+      if($password === $confirmpassword) {
 
-      if($password === $confirmpassword){
+        // Verificar se o e-mail já está cadastrado no sistema
+        if($userDao->findByEmail($email) === false) {
 
-        // Verificar se o email ja está cadastrado no sistema 
-        if($userDao->findByEmail($email) === false){
-          
           $user = new User();
 
           // Criação de token e senha
@@ -47,26 +46,47 @@
           $auth = true;
 
           $userDao->create($user, $auth);
+
+        } else {
+          
+          // Enviar uma msg de erro, usuário já existe
+          $message->setMessage("Usuário já cadastrado, tente outro e-mail.", "error", "back");
+
         }
-        else{
-          // Enviar uma msg de erro, de email ja cadastrado 
-          $message->setMessage("Email ja utilizado, tente outro email", "error", "back");
-        }
-      
-      }
-      else {
+
+      } else {
+
         // Enviar uma msg de erro, de senhas não batem
-        $message->setMessage("As senhas não são iguais", "error", "back");
+        $message->setMessage("As senhas não são iguais.", "error", "back");
+
       }
-    
-    }
-    else{
+
+    } else {
+
       // Enviar uma msg de erro, de dados faltantes
-      $message->setMessage("Por favor, preencha todos os campos", "error", "back");
+      $message->setMessage("Por favor, preencha todos os campos.", "error", "back");
 
     }
-  
-  }
-  else if($type ==="login"){
+
+  } else if($type === "login") {
+
+    $email = filter_input(INPUT_POST, "email");
+    $password = filter_input(INPUT_POST, "password");
+
+    // Tenta autenticar usuário
+    if($userDao->authenticateUser($email, $password)) {
+
+      $message->setMessage("Seja bem-vindo!", "success", "editprofile.php");
+
+    // Redireciona o usuário, caso não conseguir autenticar
+    } else {
+
+      $message->setMessage("Usuário e/ou senha incorretos.", "error", "back");
+
+    }
+
+  } else {
+
+    $message->setMessage("Informações inválidas!", "error", "index.php");
 
   }
--- dao/UserDAO.php
+++ dao/UserDAO.php
@@ -49,8 +49,34 @@
       }
     }
 
-    public function update(User $user){
+    public function update(User $user, $redirect = true){
 
+      $stmt = $this->conn->prepare("UPDATE users SET
+
+        name = :name,
+        lastname = :lastname,
+        email = :email,
+        image = :image,
+        bio = :bio,
+        token = :token
+        WHERE id = :id      
+      ");
+
+      $stmt->bindParam(":name", $user->name);
+      $stmt->bindParam(":lastname", $user->lastname);
+      $stmt->bindParam(":email", $user->email);
+      $stmt->bindParam(":image", $user->image);
+      $stmt->bindParam(":bio", $user->bio);
+      $stmt->bindParam(":token", $user->token);
+       $stmt->bindParam(":id", $user->id);
+
+      $stmt->execute();
+
+      if($redirect){
+        
+        // Redirecionar para o perfil do usuário
+        $this->message->setMessage("Dados atualizados com sucesso", "success", "editprofile.php");
+      }
     }
 
     public function verifyToken($protected = false){
@@ -90,7 +116,36 @@
       }
     }
 
-    public function authenticateUser($email,$password){
+     public function authenticateUser($email, $password) {
+
+      $user = $this->findByEmail($email);
+
+      if($user) {
+
+        // Checar se as senhas batem
+        if(password_verify($password, $user->password)) {
+
+          // Gerar um token e inserir na session
+          $token = $user->generateToken();
+
+          $this->setTokenToSession($token, false);
+
+          // Atualizar token no usuário
+          $user->token = $token;
+
+          $this->update($user, false);
+
+          return true;
+
+        } else {
+          return false;
+        }
+
+      } else {
+
+        return false;
+
+      }
 
     }
 
